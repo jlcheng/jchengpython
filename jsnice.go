8c3eb830021eca0d52fc7b6306db9a23b8499e76
John Cheng <johnlicheng@gmail.com>
2018-03-17 13:19:42 -0700

    fix bug where jsnice didn't work with arrays

--- jsnice.go
+++ jsnice.go
@@ -3,16 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	dec := json.NewDecoder(os.Stdin)
+	var in io.Reader
+	if len(os.Args) > 1 {
+		fname := os.Args[1]
+		var err error
+		if in, err = os.Open(fname); err != nil {
+			fmt.Fprintf(os.Stderr, "cannot open file %v: %v\n", fname, err)
+		}
+	} else {
+		in = os.Stdin
+	}
+	dec := json.NewDecoder(in)
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
-	buf := make(map[string]interface{})
+	var buf interface{}
 	if err := dec.Decode(&buf); err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "cannot decode input:%v\n", err)
 		os.Exit(1)
 	} else {
 		enc.Encode(buf)
